7ad8e96a1781c33a2409c619dc20c186147767bd
Thomas <thomas.florent31@gmail.com>
2019-07-11 16:20:01 +0200

    upd

--- server.js
+++ server.js
@@ -1,62 +1,48 @@
 const mongo = require('mongodb').MongoClient;
-const client = require('socket.io').listen(27017).sockets;
+const client = require('socket.io').listen(4000).sockets;
 
-//Connect to mongo
-
-mongo.connect('mongodb://127.0.0.1:8080/becodemessenger', {
-    useNewUrlParser: true
-}, (err, db) => {
-    if (err) {
+// Connect to mongo
+mongo.connect('mongodb://127.0.0.1/mongochat', { useNewUrlParser: true }, function(err, db){
+    if(err){
         throw err;
     }
 
-    console.log('MongoDB connected...')
+    console.log('MongoDB connected...');
 
-    //Connect to Socket.ignore-none
-    client.on('connection', (socket) => {
+    // Connect to Socket.io
+    client.on('connection', function(socket){
         let chat = db.collection('chats');
 
-
         // Create function to send status
-
-        sendStatus = function (s) {
+        sendStatus = function(s){
             socket.emit('status', s);
         }
 
-        //Get chats from mongo collection
-
-        chat.find().limit(100).sort({
-            _id: 1
-        }).toArray(function (err, res) {
-            if (err) {
-                throw err
+        // Get chats from mongo collection
+        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err;
             }
-            //Emit the messages
 
-            socket.emit('Output', res);
+            // Emit the messages
+            socket.emit('output', res);
         });
 
-        //Handle input events
-
-        socket.on('input', function (data) {
+        // Handle input events
+        socket.on('input', function(data){
             let name = data.name;
             let message = data.message;
 
-            //Check for name and message
-
-            if (name == "" || message == "") {
-                //send error status
-                sendStatus("Please enter a name and message")
+            // Check for name and message
+            if(name == '' || message == ''){
+                // Send error status
+                sendStatus('Please enter a name and message');
             } else {
-                //Insert message
-                chat.insert({
-                    name: name,
-                    message: message
-                }, function () {
+                // Insert message
+                chat.insert({name: name, message: message}, function(){
                     client.emit('output', [data]);
 
                     // Send status object
-
                     sendStatus({
                         message: 'Message sent',
                         clear: true
@@ -66,12 +52,12 @@ mongo.connect('mongodb://127.0.0.1:8080/becodemessenger', {
         });
 
         // Handle clear
-        socket.on('Clear', function (data) {
-            //Remove all chats from collection
-            chat.remove({}, function () {
-                //Emit cleared
-                socket.emit('Cleared')
-            })
-        })
+        socket.on('clear', function(data){
+            // Remove all chats from collection
+            chat.remove({}, function(){
+                // Emit cleared
+                socket.emit('cleared');
+            });
+        });
     });
 });
